f321dbeea5df9f920ab2798a38eb55742cc6d4b4
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-02-29 01:35:16 -0300

    Changed the return on controllers file

--- src/controllers.php
+++ src/controllers.php
@@ -5,12 +5,14 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use App\Bundle\SilexRouteExtension;
 
 // Set up container
 $container = new ContainerBuilder();
 $container->registerExtension(new SilexRouteExtension);
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config/'));
 $loader->load('routes.yml');
+
 $app['container'] = $container;
 
 $app->mount('/', include 'myApp.php');
@@ -24,5 +26,5 @@ $app->error(function (\Exception $e, $code) use ($app) {
 
     $page = 404 == $code ? '404.html' : '500.html';
 
-    return new Response($app['twig']->render($page, array('code' => $code)), $code);
+    return new Response(json_encode(array('message' => $e->getMessage())));
 });
