9f1b43f3e13d0d14c2b508849577bdd266145f70
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-02-29 02:41:57 -0300

    Developed the methods on owner

--- src/Crawler/Business/Service/GithubOwnerService.php
+++ src/Crawler/Business/Service/GithubOwnerService.php
@@ -2,6 +2,8 @@
 
 namespace App\Bundle\Crawler\Business\Service;
 
+use App\Bundle\Crawler\Entity\GithubOwner;
+
 class GithubOwnerService
 {
 
@@ -15,6 +17,42 @@ class GithubOwnerService
     public function getRepository()
     {
         $repository = $this->em->getRepository('App\Bundle\Crawler\Entity\GithubOwner');
+        return $repository;
+    }
+
+    public function getByProvider($provider)
+    {
+        $entities = $this->getRepository()->findOneBy([
+            'providerCode' => $provider
+        ]);
+
+        return $entities;
+    }
+
+    public function createByRequest($request)
+    {
+        $entity = new GithubOwner();
+        $entity->setLogin($request->login);
+        $entity->setProviderCode($request->id);
+        $entity->setUrl($request->url);
+        $entity->setHomeUrl($request->homeUrl);
+        $entity->setType($request->type);
+
+        $entity = $this->insertOrUpdate($entity);
+
+        return $entity;
+    }
+
+    private function insertOrUpdate($entity)
+    {
+        if(!$entity->getId()) {
+            $entity = $this->em->persist($entity);
+            $entity = $this->em->flush();
+        }else{
+            $entity = $this->em->flush();
+        }
+
+        return $entity;
     }
 
 }
